4aa52f047ae5cb402c8054638354517d85e8e62a
Kyle Balousek <kbalouse@umich.edu>
2015-11-21 17:00:58 -0500

    Fingertip centroids

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -28,15 +28,19 @@ pair<vector<Point>, vector<Point>> CurvedPoints(const vector<Point>& Contour, in
 	for (int i = Diff; i < Contour.size() - Diff; i++) {
 		Point a = Contour[i] - Contour[i + Diff];
 		Point b = Contour[i] - Contour[i - Diff];
-		double Angle = ComputeAngle(a,b);
+		Point c = Contour[i] - Contour[i - Diff];
+		Point d = Contour[i + Diff] - Contour[i];
+		double Angle = ComputeAngle(a, b);
 		if (Angle < Theta) {
-			Point3d a3d(a);
-			Point3d b3d(b);
-			Point3d Cross = a3d.cross(b3d);
-			if (Cross.z >= 0)
+			Point3d c3d(c);
+			Point3d d3d(d);
+			Point3d Cross = c3d.cross(d3d);
+			if (Cross.z >= 0){
 				Curves.first.push_back(Contour[i]);
-			else
+			}
+			else{
 				Curves.second.push_back(Contour[i]);
+			}
 		}
 	}
 	return Curves;
@@ -101,6 +105,38 @@ Mat SkinSegment(const Mat& Frame)
 	return BlurredFrame;
 }
 
+
+vector<Point> Cluster(const vector<Point>& Points, double DistThreshold)
+{
+	vector<Point> Centroids;
+
+	if (Points.size() == 0) return Centroids;
+
+	Point Sum = Points[0];
+	int Count = 1;
+	for (int i = 0; i < Points.size() - 1; i++) {
+		double dist = norm(Points[i + 1] - Points[i]);
+		if (dist < DistThreshold) {
+			Sum += Points[i + 1];
+			Count++;
+		} else {
+			if (Count > 0) {
+				Point Cluster;
+				Cluster.x = (double) Sum.x / Count;
+				Cluster.y = (double) Sum.y / Count;
+
+				Centroids.push_back(Cluster);
+			}
+			
+			Sum = Points[i + 1];
+			Count = 1;
+		}
+	}
+
+	return Centroids;
+}
+
+
 int main(int argc, char** argv)
 {
 	VideoCapture VideoStream(CV_CAP_ANY);
@@ -120,24 +156,40 @@ int main(int argc, char** argv)
 		vector<vector<Point>> Contours = GetContours(Threshold);
 		vector<vector<Point>> CurvesUp;
 		vector<vector<Point>> CurvesDown;
+		vector<vector<Point>> ClustersUp;
+		vector<vector<Point>> ClustersDown;
 		for (auto& Contour : Contours) {
-			auto Pts = CurvedPoints(Contour, 20, 45);
-			CurvesUp.push_back(Pts.first);
-			CurvesDown.push_back(Pts.second);
+			auto Pts = CurvedPoints(Contour, 15, 60);
+			CurvesUp.push_back(Pts.second);
+			CurvesDown.push_back(Pts.first);
+			ClustersUp.push_back(Cluster(Pts.second, 10));
+			ClustersDown.push_back(Cluster(Pts.first, 10));
 		}
 
 		for (auto& Component : CurvesUp) {
 			for (auto& Point : Component) {
-				circle(Frame, cvPoint(Point.x, Point.y), 5, CV_RGB(255, 0, 0), -1, 8, 0);
+				circle(Frame, cvPoint(Point.x, Point.y), 2, CV_RGB(255, 0, 0), -1, 8, 0);
 			}
 		}
-
+		
 		for (auto& Component : CurvesDown) {
 			for (auto& Point : Component) {
-				circle(Frame, cvPoint(Point.x, Point.y), 5, CV_RGB(0, 0, 255), -1, 8, 0);
+				circle(Frame, cvPoint(Point.x, Point.y), 2, CV_RGB(0, 0, 255), -1, 8, 0);
 			}
 		}
-
+		/*
+		for (auto& Component : ClustersUp) {
+			for (auto& Point : Component) {
+				circle(Frame, cvPoint(Point.x, Point.y), 5, CV_RGB(0, 0, 0), -1, 8, 0);
+			}
+		}
+		*/
+		for (auto& Component : ClustersDown) {
+			for (auto& Point : Component) {
+				circle(Frame, cvPoint(Point.x, Point.y), 5, CV_RGB(0, 0, 0), -1, 8, 0);
+			}
+		}
+		
 		/*
 		Mat ContourImg;
 		for (int i = 0; i < Contours.size(); i++)
@@ -157,8 +209,8 @@ int main(int argc, char** argv)
 			convexHull(Polys[i], Hulls[i]);
 		}
 
-		for (int i = 0; i < Contours.size(); i++)
-			drawContours(Frame, Hulls, i, Scalar(255, 255, 255), 1, 8);
+		//for (int i = 0; i < Contours.size(); i++)
+		//	drawContours(Frame, Hulls, i, Scalar(255, 255, 255), 1, 8);
 		/*
 		for (auto& Hull : Hulls) {
 			for (auto& Point : Hull) {
